d8b1bd905f4e4b90cb2cd01190b101e014bf8813
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-05-15 14:31:27 +0200

    use gonzales for less parsing

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-var less = require('less');
+var gonzales = require('gonzales-pe');
+
+function walk(node, fn) {
+    fn(node);
+    if (node.content && node.content.forEach) {
+        node.content.forEach(function(child) {
+            walk(child, fn);
+        });
+    }
+}
 
 function symdiffLESS(lessString) {
-    return less.render(lessString, {
-        async: false
-    }, function (e, output) {
-        //TODO
-        console.log(e, output);
-        return output.css;
+    var ast;
+    try {
+        ast = gonzales.parse(lessString, {
+            syntax: 'less'
+        });
+    } catch(err) {
+        return [];
+    }
+    if (!ast) {
+        return [];
+    }
+    var classes = [];
+    walk(ast, function(node) {
+        if (node.type === 'class') {
+            node.content.forEach(function(clazzContent) {
+                if (clazzContent.type === 'ident') {
+                    classes.push(clazzContent.content);
+                }
+            });
+        }
     });
+    return classes;
 }
 
 module.exports = symdiffLESS;
